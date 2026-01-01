f3bb464f67ebc06d84a2158f9348e74692e27108
Qing 'Matt' Zhang <mattz.ca@gmail.com>
2026-01-01 12:29:26 -0800

    Update missingElement.java

--- array/missingElement.java
+++ array/missingElement.java
@@ -1,15 +1,17 @@
 /*
 Missing Elements in Sorted Array
 Given a sorted array A of unique numbers, find the K-th missing number starting from the leftmost number of the array.
+
+lc 1060
 */
 
 public int missingElement(int[] nums, int k) {
-    int i = 0, left = k;
-    while((i < nums.length -1) && (nums[i] + left >= nums[i+1])){
-        left = left - (nums[i+1] - nums[i] - 1);
+    int i = 0, remain = k;
+    while((i < nums.length -1) && (nums[i] + remain >= nums[i+1])){
+        remain = remain - (nums[i+1] - nums[i] - 1);
         i++;
     }
-    return nums[i] + left;
+    return nums[i] + remain;
 }
 
 
@@ -24,8 +26,9 @@ int missing(int idx, int[] nums) {
 public int missingElement(int[] nums, int k) {
     int n = nums.length;
     // If kth missing number is larger than the last element of the array
+    // it'll be beyond the last element of this array, compute directly.
     if (k > missing(n - 1, nums))
-        return nums[n - 1] + k - missing(n - 1, nums);
+        return nums[n - 1] + k - missing(n - 1, nums);b
 
     int left = 0, right = n - 1, mid;
     // find left = right index such that missing(left - 1) < k <= missing(left)
